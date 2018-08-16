d365d35aa26b3f8600c0992aaeb37fbed3d7d795
Victor Korir <vkkorir@gmail.com>
2018-08-16 00:00:30 -0700

    removing prettier config file && archive zip

---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "/Users/Victor/Documents/Developer/client/personal-website/src/layouts/index.js"
+  import data from "/Users/Victor/Documents/Developer/client/personal-website/.cache/json/layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
